59cdc17626727c47e8984f9cb137db38e7495bcd
poprox <hudec.matyas.2006@gmail.com>
2023-09-17 07:24:04 +0200

    Initial Commit

---
+++ auth_server.py
@@ -0,0 +1,99 @@
+from flask import Flask, render_template, request
+from ossapi import Ossapi
+import requests
+import json
+import discord
+import asyncio
+
+
+class AuthServer:
+
+    def __init__(self, bot: discord.Bot, verifyUpdateChannel, serverId, loop) -> None:
+        self._bot = bot
+        self._verify_update_channel_id = verifyUpdateChannel
+        self._server_id = serverId
+        self._loop = loop
+
+        self._load_users()
+
+    def index(self):
+        return render_template('egg.html')
+
+    async def oauth_callback(self):
+        authorization_code = request.args.get('code')
+        discord_id = request.args.get('state')
+
+        oauth_response = self._get_oauth_response(authorization_code)
+
+        if oauth_response.status_code != 200:
+            return render_template('error.html')
+
+        access_token = oauth_response.json()['access_token']
+
+        osu_id = self._get_osu_id(access_token)
+
+        verify_update_channel = self._bot.get_channel(self._verify_update_channel_id)
+        guild = self._bot.get_guild(self._server_id)
+        member = guild.get_member(int(discord_id))
+
+        if self._is_user_verified(discord_id, osu_id):
+            asyncio.run_coroutine_threadsafe(verify_update_channel.send(f"User has tried to verify, but has already been verified before. <@{discord_id}> https://osu.ppy.sh/users/{osu_id}"), self._loop)
+            asyncio.run_coroutine_threadsafe(member.send("You have already been verified before\nIf this is an alt account, go away\nIf this is a mistake, contact <@509028328947056693> in DMs"), self._loop)
+
+            return render_template('already.html')
+        
+        dictionary = {
+            "discord_name": f"{member.name}",
+            "discord_id": f"{discord_id}",
+            "osu_id": f"{osu_id}"
+        }
+
+        await self._add_roles(member, osu_id, guild, verify_update_channel)
+
+        self._write_json(dictionary)
+
+        return render_template('complete.html')
+
+    def _write_json(self, new_data, filename='verified.json'):
+        with open(filename, 'r+') as file:
+            self._users = json.load(file)
+            self._users["verified_users"].append(new_data)
+            file.seek(0)
+
+            json.dump(self._users, file, indent=4)
+
+    def _is_user_verified(self, discord_id, osu_id) -> bool:
+        for user in self._users['verified_users']:
+            if discord_id == user['discord_id'] or osu_id == user['osu_id']:
+                return True
+        
+        return False
+    
+    def _load_users(self, filename='verified.json'):
+        with open(filename) as file:
+            self._users = json.load(file)
+
+    async def _add_roles(self, member: discord.Member, osu_id, guild: discord.Guild, update_channel: discord.TextChannel):
+        asyncio.run_coroutine_threadsafe(member.add_roles(discord.utils.get(guild.roles, name="Member")), self._loop)
+        asyncio.run_coroutine_threadsafe(update_channel.send(f"<@{member.id}> Has successfully verified their account. https://osu.ppy.sh/users/{osu_id}"), self._loop)
+        asyncio.run_coroutine_threadsafe(member.send("Successfully verified\nEnjoy the server!"), self._loop)
+
+    def _get_osu_id(self, access_token) -> int:
+        return Ossapi(
+            client_id='24127',
+            client_secret='qJHR0ejwuxdJQGwaD5JKlLvoMvfIAahiU6vcmPDT',
+            redirect_uri='https://verify.mafiosu.net/oauth-callback',
+            scopes=["identify"],
+            access_token=access_token
+        ).get_me().id
+    
+    def _get_oauth_response(self, authorization_code) -> requests.Response:
+        post_data = {
+            'client_id': '24127',
+            'client_secret': 'qJHR0ejwuxdJQGwaD5JKlLvoMvfIAahiU6vcmPDT',
+            'code': authorization_code,
+            'grant_type': 'authorization_code',
+            'redirect_uri': 'https://verify.mafiosu.net/oauth-callback'
+        }
+
+        return requests.post('https://osu.ppy.sh/oauth/token', data=post_data)
---
+++ index.py
@@ -0,0 +1,113 @@
+import discord
+
+from flask import Flask
+import asyncio
+
+# Import .py files
+import auth_server
+
+import restriction_manager
+
+
+TOKEN = 'MTEyMzkyMjkxNzIzMTcxMDI0OQ.GSJInG.UeyiV60GpywsX4kza8RKcFu_uwldy4Yzs75qFI'
+
+intents = discord.Intents.all()
+intents.members = True
+
+bot = discord.Bot(intents=intents)
+
+server_id = 1141718152946925679 
+
+# Custom
+@bot.slash_command(guild_id=server_id,
+                   name="embed",
+                   description="Custom Embed")
+async def shame(ctx, title, field_name, field_text, footer_text):
+  moderator = discord.utils.get(ctx.guild.roles, name="Moderator")
+  head = discord.utils.get(ctx.guild.roles, name="Head")
+  host = discord.utils.get(ctx.guild.roles, name="Host")
+  if moderator in ctx.author.roles or head in ctx.author.roles or host in ctx.author.roles:
+    await ctx.respond("Sent!", delete_after=0.05, ephemeral=True)
+    shame = discord.Embed(title=f"{title}", color=discord.Color.red())
+    shame.add_field(name=f"{field_name}", value=f"{field_text}")
+    shame.set_footer(text=f"{footer_text}")
+    await ctx.send(embed=shame)
+  else:
+    await ctx.respond("You don't have rights to use this command.",
+                      ephemeral=True)
+
+
+# Message
+@bot.slash_command(guild_id=server_id,
+                   name="message",
+                   description="Custom Message")
+async def message(ctx, message):
+  moderator = discord.utils.get(ctx.guild.roles, name="Moderator")
+  head = discord.utils.get(ctx.guild.roles, name="Head")
+  host = discord.utils.get(ctx.guild.roles, name="Host")
+  if moderator in ctx.author.roles or head in ctx.author.roles or host in ctx.author.roles:
+    await ctx.respond("Sent!", delete_after=0.05, ephemeral=True)
+    await ctx.send(f"{message}")
+  else:
+    await ctx.respond("You don't have rights to use this command.",
+                      ephemeral=True)
+
+
+# Verify 
+
+# Flask Web Server Setup
+app = Flask(__name__, template_folder='./websites/')
+app2 = Flask(__name__, template_folder='./webserver/')
+server = auth_server.AuthServer(bot, 1141730722189672518, server_id, asyncio.get_event_loop())
+
+@app.route('/')
+def index():
+    return server.index()
+
+@app2.route('/')
+def index():
+    return 'Hi, this page is not done yet, consider it an easter egg'
+
+# Verify
+
+CLIENT_ID = '24127'
+CLIENT_SECRET = 'qJHR0ejwuxdJQGwaD5JKlLvoMvfIAahiU6vcmPDT'
+
+@app.route('/oauth-callback', methods=['GET', 'POST'])
+async def oauth_callback():
+  return await server.oauth_callback()
+
+
+class Button(discord.ui.View):
+    def __init__(self):
+        super().__init__(timeout=None)
+    
+    @discord.ui.button(label="Verify!", style=discord.ButtonStyle.primary, emoji="✅", custom_id="verify_button1")
+    async def button_callback(self, button, interaction):
+        button.disabled = True
+        REDIRECT_URI = f'https://verify.mafiosu.net/oauth-callback&state={interaction.user.id}'
+        auth_url = f'https://osu.ppy.sh/oauth/authorize?client_id={CLIENT_ID}&response_type=code&scope=identify&redirect_uri={REDIRECT_URI}'
+        await interaction.response.send_message(auth_url, ephemeral=True)
+
+
+
+@bot.event
+async def on_message(ctx):
+  if (ctx.channel.id == 1141718898622861424 and ctx.author.id != 1123922917231710249 and ctx.content != "/verify"):
+    await ctx.delete()
+
+
+@bot.event
+async def on_ready():
+  print(f"Logged in.")
+  restriction_manager.timedRestriction.start()
+  #   await bot.get_channel(1141718898622861424).send("Click the button to verify!", view=Button())
+  bot.add_view(Button())
+
+if __name__ == '__main__':
+    import threading
+    threading.Thread(target=app.run, kwargs={'host': '0.0.0.0', 'port': 25249}).start()
+    threading.Thread(target=app2.run, kwargs={'host': '0.0.0.0', 'port': 25208}).start()
+    
+    # Run the Discord bot
+    bot.run(TOKEN)
