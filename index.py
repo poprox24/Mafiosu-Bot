ded772259dc3da2a32b0664c553fa5defd15a9db
poprox <hudec.matyas.2006@gmail.com>
2023-11-01 15:22:28 +0100

    e

--- index.py
+++ index.py
@@ -10,7 +10,6 @@ import auth_server
 
 import restriction_manager
 
-import json
 import typing
 from datetime import datetime, timedelta
 
@@ -22,6 +21,8 @@ intents.members = True
 bot = discord.Bot(intents=intents)
 
 server_id = 1141718152946925679 
+
+restriction_channel = bot.get_channel(1141730974636458037)
 
 #restrict
 @bot.slash_command(guild_id=1141718152946925679,
@@ -195,6 +196,71 @@ async def clearrestrict(ctx,
 
     with open("restriction.json", "w") as file:
       json.dump(data, file, indent=4)
+
+@tasks.loop(seconds=60.0)
+async def timedRestriction():
+
+  currenttime = datetime.now().strftime("%d/%m %H:%M")
+  time = datetime.now().strptime(currenttime, "%d/%m %H:%M")
+  print(time.strftime("%d/%m %H:%M") + " Current time")
+
+  print("Restricted Users:")
+
+  file = open("restriction.json")
+
+  data = json.load(file)
+
+  for i in data["restricted_users"]:
+
+    unrestricttime = i["time"]
+    id = i["id"]
+    name: discord.Member = i["name"]
+    unrestrictedtime = i["restricttime"]
+    restrictedtype = i["type"]
+    reason = i["reason"]
+
+    server = bot.get_guild(int(server_id))
+
+    print(name)
+
+    name = name.replace("#", "")
+    name = name.replace("0", "")
+
+    member = server.get_member_named(name)
+
+
+    if time.strftime("%d/%m %H:%M") >= unrestricttime:
+
+      if (restrictedtype == "1"):
+        await member.add_roles(discord.utils.get(server.roles, name="Member"))
+        await member.add_roles(discord.utils.get(server.roles, name="Had Restriction"))
+        await member.remove_roles(discord.utils.get(server.roles, name="Restricted"))
+      elif (restrictedtype == "2"):
+        await member.add_roles(discord.utils.get(server.roles, name="Member"))
+        await member.add_roles(discord.utils.get(server.roles, name="Had Restriction +"))
+        await member.remove_roles(discord.utils.get(server.roles, name="Restricted +"))
+
+      if (restrictedtype == "1"):
+        restrictedtype = "Restricted"
+      else:
+        restrictedtype = "Restricted +"
+
+      message = discord.Embed(title="User UNestricted",
+                              color=discord.Color.blue())
+      message.add_field(
+        name=f"User was unrestricted",
+        value=
+        f"<@{id}> Was Unrestricted\n\nWhen Restricted: **{unrestrictedtime}**\nWhen Unrestricted: **{unrestricttime}**\n\nType: **{restrictedtype}**\n\nReason: **{reason}**"
+      )
+      await restriction_channel.send(embed=message)
+
+      restricted_users = data['restricted_users']
+      filtered_users = [user for user in restricted_users if user != i]
+      data['restricted_users'] = filtered_users
+
+      with open('restriction.json', 'w') as file:
+        json.dump(data, file, indent=4)
+
 
 # Custom
 @bot.slash_command(guild_id=server_id,
